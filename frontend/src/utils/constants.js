bd154f44285ccf2b839f2ef62afc13ba18614acc
Olgauk <ms.olgasmith.jj@mail.ru>
2026-02-28 17:30:43 +0300

    add api

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = 'http://localhost:4000';
+export const URL = 'https://api.nodeok2.nomorepartiessite.ru';
 
 export const EMAIL_REGULAR =
   /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/;
